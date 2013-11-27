1ee9eecbc2a866f67dd65d4b4897f97e408a62d7
matteprg <spc.eubanks.matthew@gmail.com>
2013-11-26 19:59:04 -0700

    My Changes

--- RoadsideAssistance/RoadsideAssistance/Controller/ApplicationController.vb
+++ RoadsideAssistance/RoadsideAssistance/Controller/ApplicationController.vb
@@ -1,16 +1,60 @@
-﻿'Purpose of this component is to provide validation for the between the gui and the datamanager
+﻿Imports System.Collections.ObjectModel
+Imports System.IO
+Imports Newtonsoft.Json
+
+
+
+'Purpose of this component is to provide validation for the between the gui and the datamanager
 'It needs the variable and contructor to that initializes them.
 Public Class ApplicationController : Inherits NotifyBase
-    Public Sub ApplicationController()
 
+    Private Const PROVIDERLISTFILE = "providers.json"
+    Private Const SERVICECALLLISTFILE = "providers.json"
+    Private Const STATUSLISTFILE = "providers.json"
+    Private Const CUSTOMERLISTFILE = "providers.json"
+    Private Const CONTACTLISTFILE = "providers.json"
+    Private _srvCall As ServiceCall
+    Public Property SrvCall() As ServiceCall
+        Get
+            Return _srvCall
+        End Get
+        Set(ByVal value As ServiceCall)
+            _srvCall = value
+            Notify("SrvCall")
+        End Set
+    End Property
+
+    Private _providerList As ObservableCollection(Of Provider)
+    Public Property ProviderList() As ObservableCollection(Of Provider)
+        Get
+            Return _providerList
+        End Get
+        Set(ByVal value As ObservableCollection(Of Provider))
+            _providerList = value
+            Notify("ProviderList")
+        End Set
+    End Property
+
+    Public Sub New()
+        ProviderList = New ObservableCollection(Of Provider)
+        InitializeProviders()
+        InitializeContacts()
+        NewServiceCall()
+    End Sub
+
+    Public Sub NewServiceCall()
+        SrvCall = New ServiceCall()
+        SrvCall.Customer = New Customer()
+        SrvCall.Customer.Location = New Location()
+        SrvCall.Customer.Location.Phones = New ObservableCollection(Of Phone)
+        SrvCall.CallLocation = New CallLocation()
+        SrvCall.Statuses = New ObservableCollection(Of Status)
     End Sub
 
     Public Sub InitializeProviders()
-
-    End Sub
-
-    Public Sub InitializeCustomers()
-
+        ProviderList.Add(New Provider With {.ID = 0, .CompanyName = "Provider1", .Website = "site1", .Email = "@email1", .Location = New Location With {.ID = 0, .Address = "123 overthere", .City = "city1", .State = "S1", .Zip = 12345}})
+        ProviderList.Add(New Provider With {.ID = 0, .CompanyName = "Provider2", .Website = "site2", .Email = "@email2", .Location = New Location With {.ID = 0, .Address = "223 overthere", .City = "city2", .State = "S2", .Zip = 12345}})
+        ProviderList.Add(New Provider With {.ID = 0, .CompanyName = "Provider3", .Website = "site3", .Email = "@email3", .Location = New Location With {.ID = 0, .Address = "323 overthere", .City = "city3", .State = "S3", .Zip = 12345}})
     End Sub
 
     Public Sub InitializeContacts()
@@ -18,23 +62,43 @@ Public Class ApplicationController : Inherits NotifyBase
     End Sub
 
     Public Sub SaveProvider()
-
+        Dim writer As TextWriter = File.CreateText(PROVIDERLISTFILE)
+        Dim json As String = JsonConvert.SerializeObject(PROVIDERLISTFILE)
+        writer.Write(json)
+        writer.Flush()
+        writer.Close()
     End Sub
 
     Public Sub SaveServiceCall()
-
+        Dim writer As TextWriter = File.CreateText(SERVICECALLLISTFILE)
+        Dim json As String = JsonConvert.SerializeObject(SERVICECALLLISTFILE)
+        writer.Write(json)
+        writer.Flush()
+        writer.Close()
     End Sub
 
     Public Sub SaveStatus()
-
+        Dim writer As TextWriter = File.CreateText(STATUSLISTFILE)
+        Dim json As String = JsonConvert.SerializeObject(STATUSLISTFILE)
+        writer.Write(json)
+        writer.Flush()
+        writer.Close()
     End Sub
 
     Public Sub SaveCustomer()
-
+        Dim writer As TextWriter = File.CreateText(CUSTOMERLISTFILE)
+        Dim json As String = JsonConvert.SerializeObject(CUSTOMERLISTFILE)
+        writer.Write(json)
+        writer.Flush()
+        writer.Close()
     End Sub
 
     Public Sub SaveContact()
-
+        Dim writer As TextWriter = File.CreateText(CONTACTLISTFILE)
+        Dim json As String = JsonConvert.SerializeObject(CONTACTLISTFILE)
+        writer.Write(json)
+        writer.Flush()
+        writer.Close()
     End Sub
 
 End Class
---
+++ RoadsideAssistance/RoadsideAssistance/Views/MainWindow.xaml.vb
@@ -0,0 +1,29 @@
+﻿Public Class MainWindow
+
+    Public _appControl As ApplicationController
+
+    Public Sub New()
+        _appControl = New ApplicationController()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.DataContext = _appControl
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As RoutedEventArgs)
+        Me.Close()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As RoutedEventArgs)
+        MessageBox.Show("Save button.")
+    End Sub
+
+    Private Sub btnPrevious_Click(sender As Object, e As RoutedEventArgs) Handles btnPrevious.Click
+
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As RoutedEventArgs) Handles btnNext.Click
+
+    End Sub
+End Class
